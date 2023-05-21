d359cb7cc20c1f5b9ea85952e0bab1fc4c8239d8
beredi <43635436+beredi@users.noreply.github.com>
2023-05-21 22:16:23 +0200

    Showing car parts

---
+++ src/components/PartsTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>Parts</h3>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Serial Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="part in parts" :key="part.id">
+          <td>{{ part.id }}</td>
+          <td>{{ part.name }}</td>
+          <td>{{ part.serial_number }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    parts: Array,
+  },
+};
+</script>
--- src/views/CarDetailsView.vue
+++ src/views/CarDetailsView.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container">
     <h2>Car Information</h2>
-    <router-link to="/" class="btn btn-info text-white"
-      ><i class="fas fa-chevron-left"></i> Back to Cars</router-link
-    >
+    <router-link to="/" class="btn btn-info text-white">
+      <i class="fas fa-chevron-left"></i> Back to Cars
+    </router-link>
+
     <template v-if="car">
       <p><strong>Name:</strong> {{ car.name }}</p>
       <p><strong>Registration Number:</strong> {{ car.registration_number }}</p>
       <p><strong>Is Registered:</strong> {{ car.is_registered }}</p>
+
+      <parts-table v-if="car.parts" :parts="car.parts"></parts-table>
     </template>
     <template v-else>
       <p>Loading car information...</p>
@@ -16,7 +19,12 @@
 </template>
 
 <script>
+import PartsTable from "@/components/PartsTable";
+
 export default {
+  components: {
+    PartsTable,
+  },
   data() {
     return {
       car: null,
