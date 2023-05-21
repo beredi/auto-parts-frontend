e92c64d080f7e01fdfa18312d2371345450025cd
beredi <43635436+beredi@users.noreply.github.com>
2023-05-21 20:41:45 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div className="container">
+    <h1>My Vue.js App with Bootstrap</h1>
+    <div className="alert alert-primary" role="alert">
+      This is a Bootstrap alert!
+    </div>
+  </div>
+</template>
